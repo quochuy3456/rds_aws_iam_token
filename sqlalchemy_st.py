b0e78e6eea363ca734fb63ba489a1952409d9b1d
huy.do1 <Quochuydo@1994>
2021-07-28 22:58:53 +0700

    #add initial

--- sqlalchemy_st.py
+++ sqlalchemy_st.py
@@ -12,7 +12,7 @@ mysql_attr_ssl_ca = "rds-combined-ca-bundle.pem"
 region_name = "ap-northeast-1a"
 host = "database-3.cgjasvizzmcb.ap-northeast-1.rds.amazonaws.com"
 port = 3306
-user = "rds_iam_user"
+user = "admin"
 passwd = "Quochuydo!1994"
 
 ssl_args = {'ssl': {'ca': mysql_attr_ssl_ca}}
@@ -29,21 +29,35 @@ print("iam_token: ", iam_token)
 #                            connect_args=ssl_args
 #                            )
 
+"""
+{
+  "Version" : "2012-10-17",
+  "Statement" :
+  [
+    {
+      "Effect" : "Allow",
+      "Action" : ["rds-db:connect"],
+      "Resource" : ["arn:aws:rds-db:ap-northeast-1:521345806971:rds_iam_user:db-Z2DELENJR7BZC6X3H76KD6GUYI/isoar"]
+    }
+  ]
+}
+"""
+
 
 def get_authentication_token():
     return passwd
 
 
 class Check:
-    # mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, passwd, host,
-    #                                                                        3306, "isoar")
-    #
-    # engine = create_engine(mysql_connection_url, pool_recycle=10)
-    mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, iam_token, host,
-                                                                           str(port), "isoar")
-    engine = create_engine(mysql_connection_url,
-                           connect_args=ssl_args,
-                           pool_recycle=10)
+    mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, passwd, host,
+                                                                           3306, "isoar")
+
+    engine = create_engine(mysql_connection_url, pool_recycle=10)
+    # mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, iam_token, host,
+    #                                                                        str(port), "isoar")
+    # engine = create_engine(mysql_connection_url,
+    #                        connect_args=ssl_args,
+    #                        pool_recycle=10)
 
     session_mysql = Session(engine, autoflush=True)
     Base.prepare(engine, reflect=True)
