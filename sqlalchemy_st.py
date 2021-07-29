4d655a1ecd1a9f601549b0eda64799dfd9e3a3ba
huy.do1 <Quochuydo@1994>
2021-07-29 20:47:53 +0700

    aaaaaaaaaaa

--- sqlalchemy_st.py
+++ sqlalchemy_st.py
@@ -17,12 +17,6 @@ passwd = "Quochuydo!1994"
 
 ssl_args = {'ssl': {'ca': mysql_attr_ssl_ca}}
 client = boto3.client('rds', region_name=region_name)
-token = client.generate_db_auth_token(DBHostname=host,
-                                      Port=port,
-                                      DBUsername=user,
-                                      Region=region_name)
-iam_token = quote_plus(token)
-print("iam_token: ", iam_token)
 
 
 def get_authentication_token():
@@ -34,22 +28,21 @@ def get_authentication_token():
     return iam_token
 
 
+mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, get_authentication_token(), host,
+                                                                       str(port), "isoar")
+engine = create_engine(mysql_connection_url,
+                       connect_args=ssl_args,
+                       pool_recycle=10)
+
+session_mysql = Session(engine, autoflush=True)
+Base.prepare(engine, reflect=True)
+
+
 class Check:
     # mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, passwd, host,
     #                                                                        3306, "isoar")
     #
     # engine = create_engine(mysql_connection_url, pool_recycle=10)
-    mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, iam_token, host,
-                                                                           str(port), "isoar")
-    engine = create_engine(mysql_connection_url,
-                           connect_args=ssl_args,
-                           pool_recycle=10)
-
-    session_mysql = Session(engine, autoflush=True)
-    Base.prepare(engine, reflect=True)
-
-    def __init__(self):
-        self.sthhere = "here"
 
     @staticmethod
     @event.listens_for(engine, "do_connect")
@@ -60,10 +53,11 @@ class Check:
         # print("cargs: ", cargs)
         cparams['passwd'] = get_authentication_token()
 
-    def run(self):
+    @staticmethod
+    def run():
         while True:
-            tenant_code = self.session_mysql.query(Base.classes.tenant).get(1).company_id
-            self.session_mysql.close()
+            tenant_code = session_mysql.query(Base.classes.tenant).get(1).company_id
+            session_mysql.close()
             print(tenant_code)
             time.sleep(2)
 
