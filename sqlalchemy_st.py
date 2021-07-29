7386a8429a7fbd8f51759938f67f9aafc04eca5a
huy.do1 <Quochuydo@1994>
2021-07-29 23:33:17 +0700

    aaaaaaaaaaa

--- sqlalchemy_st.py
+++ sqlalchemy_st.py
@@ -46,6 +46,8 @@ def provide_token(dialect, conn_rec, cargs, cparams):
     print("conn_rec: ", conn_rec)
     print("cargs: ", cargs)
     cparams['passwd'] = get_authentication_token()
+    session_mysql = Session(engine, autoflush=True)
+    Base.prepare(engine, reflect=True)
 
 
 def run():
