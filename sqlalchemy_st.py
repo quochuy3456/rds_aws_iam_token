9b276c7bc33315d83c099148f39e3d8fb8a77fdb
huy.do1 <Quochuydo@1994>
2021-07-30 16:40:11 +0700

    aaaaaaaaaaa

--- sqlalchemy_st.py
+++ sqlalchemy_st.py
@@ -12,8 +12,8 @@ mysql_attr_ssl_ca = "./rds-ca-2019-root.pem"
 region_name = "ap-northeast-1"
 host = "database-1.cgjasvizzmcb.ap-northeast-1.rds.amazonaws.com"
 port = 3306
-# user = "rds_iam_user"
-user = "admin"
+user = "rds_iam_user"
+# user = "admin"
 passwd = "Quochuydo!1994"
 
 ssl_args = {'ssl': {'ca': mysql_attr_ssl_ca}}
@@ -27,8 +27,8 @@ def get_authentication_token():
                                           DBUsername=user,
                                           Region=region_name)
     iam_token = quote_plus(token)
-    # return iam_token
-    return passwd
+    return iam_token
+    # return passwd
 
 
 token = get_authentication_token()
