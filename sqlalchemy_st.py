f132b38c96c7e65d46f583ca0b655a66942dbac0
huy.do1 <Quochuydo@1994>
2021-08-10 14:53:54 +0700

    aaaaaaaaaaa

--- sqlalchemy_st.py
+++ sqlalchemy_st.py
@@ -8,34 +8,35 @@ import time
 
 Base = automap_base()
 
-mysql_attr_ssl_ca = "./rds-combined-ca-bundle.pem"
-region_name = "us-east-2"
-host = "isoar-database-1.cqxkpsrs9pdj.us-east-2.rds.amazonaws.com"
+########## Config ##################
+mysql_attr_ssl_ca = "/var/app/isoar/rds-ca-2019-root.pem"
+region_name = "ap-northeast-1"
+host = "isoar-dev-ual-db.ctbfofok8p3r.ap-northeast-1.rds.amazonaws.com"
 port = 3306
-user = "rds_iam_user"
-# user = "isoaradmin"
-# passwd = "NTQ202107"
+user = "isoarusr"
+database_name = "isoar_dev_ntq_db"
+####################################
 
-ssl_args = {'ssl': {'ca': mysql_attr_ssl_ca, "ENABLE_CLEARTEXT_PLUGIN": 1}}
 
+ssl_args = {'ssl': {'ca': mysql_attr_ssl_ca}}
 client = boto3.client('rds', region_name=region_name)
 
-
+# GEN token
 def get_authentication_token():
     token = client.generate_db_auth_token(DBHostname=host,
                                           Port=port,
                                           DBUsername=user,
                                           Region=region_name)
     iam_token = quote_plus(token)
-    print(iam_token)
     return iam_token
-    # return passwd
 
 
 token = get_authentication_token()
 
-mysql_connection_url = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, token, host,
-                                                                               str(port), "isoar_database_1")
+mysql_connection_url = 'mysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(user, token, host,
+                                                                       str(port), database_name)
+
+# Create engine
 engine = create_engine(mysql_connection_url,
                        connect_args=ssl_args,
                        pool_recycle=6)
@@ -44,43 +45,19 @@ session_mysql = Session(engine, autoflush=True)
 Base.prepare(engine, reflect=True)
 
 
+# Check and update token before give connection
 @event.listens_for(engine, "do_connect")
 def provide_token(dialect, conn_rec, cargs, cparams):
     print("cparams: ", cparams)
     cparams['passwd'] = get_authentication_token()
 
 
+# Run test (get connection
 def run():
-    time_count = 0
     while True:
-        tenant_code = session_mysql.query(Base.classes.tenant).get(1).company_id
+        tenant_code = session_mysql.query(Base.classes.tenants).get(1).tenant_id
         session_mysql.close()
         print(tenant_code)
-        time_count += 1
-        print(time_count)
         time.sleep(2)
 
-
 run()
-
-
-
-# import mysql.connector
-# from mysql.connector.constants import ClientFlag
-#
-# config = {
-#     'user': user,
-#     'password': get_authentication_token(),
-#     'host': host,
-#     'client_flags': [ClientFlag.SSL],
-#     'ssl_ca': mysql_attr_ssl_ca,
-# }
-#
-# print(config)
-#
-# cnx = mysql.connector.connect(**config)
-# cur = cnx.cursor(buffered=True)
-# cur.execute("SHOW STATUS LIKE 'Ssl_cipher'")
-# print(cur.fetchone())
-# cur.close()
-# cnx.close()
